2d4b9008e67ec6176c5125a0bce715ea21f6f018
Inna <khablenko.id@gmail.com>
2024-10-27 10:54:44 +0300

    html_fix

--- main_app/views.py
+++ main_app/views.py
@@ -27,4 +27,25 @@ def ratePrediction(request):
     data = {'data':{'PredictedRating':PredictedRating, 'attendance':attendance, 'hours_studied':hours_studied, 'sleep_hours':sleep_hours, 
     'physical_activity':physical_activity, 'home_distance':home_distance}}
     return render(request, 'neural_predict_page.html', data)
+
+def user_ratePrediction(request):
+    attendance = request.GET.get('attendance','')
+    print(attendance)
+    hours_studied = request.GET.get('hours_studied','')
+    print(hours_studied)
+    sleep_hours = request.GET.get('sleep_hours','')
+    print(sleep_hours)
+    physical_activity = request.GET.get('physical_activity','')
+    print(physical_activity)
+    home_distance = request.GET.get('home_distance','')
+    print(home_distance)
+    if attendance != "" and hours_studied != "" and sleep_hours != "" and physical_activity != "" and home_distance != "":
+        
+        PredictedRating = neuralCalc(attendance, hours_studied, sleep_hours, 
+        physical_activity, home_distance)
+    else:
+        PredictedRating = ""
+    data = {'data':{'PredictedRating':PredictedRating, 'attendance':attendance, 'hours_studied':hours_studied, 'sleep_hours':sleep_hours, 
+    'physical_activity':physical_activity, 'home_distance':home_distance}}
+    return render(request, 'user_page.html', data)
                   
--- submittion/urls.py
+++ submittion/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main_app.views import ratePrediction
+from main_app.views import ratePrediction, user_ratePrediction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ratePrediction, name='mainForm'),
+    path('user/', user_ratePrediction, name='userForm'),
 ]
